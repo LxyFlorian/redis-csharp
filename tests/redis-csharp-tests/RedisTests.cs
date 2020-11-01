63b1c15a30d2941919d29cf01824903939defaea
lxyflorian <florianelie8@gmail.com>
2020-11-01 15:53:29 +0100

    add tests for Redis.cs and rename class test in RedisTests

---
+++ tests/redis-csharp-tests/RedisTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using redis_csharp.src;
+
+namespace redis_csharp_tests
+{
+    [TestClass]
+    public class RedisCSharpTests
+    {
+        [TestMethod]
+        public void RedisHostWorks()
+        {
+            Redis redis = new Redis();
+            Assert.AreEqual("localhost", redis.Host);
+        }
+
+        [TestMethod]
+        public void RedisPortWorks()
+        {
+            Redis redis = new Redis();
+            Assert.AreEqual(6379, redis.Port);
+        }
+
+        [TestMethod]
+        public void SetPortWorks()
+        {
+            Redis redis = new Redis(5555);
+            Assert.AreEqual(5555, redis.Port);
+        }
+
+        [TestMethod]
+        public void SetHostWorks()
+        {
+            Redis redis = new Redis("redis.mywebsite.com");
+            Assert.AreEqual("redis.mywebsite.com", redis.Host);
+        }
+
+        [TestMethod]
+        public void SetHostSetPortWorks()
+        {
+            Redis redis = new Redis("redis.mywebsite.com", 5555);
+            Assert.AreEqual("redis.mywebsite.com", redis.Host);
+            Assert.AreEqual(5555, redis.Port);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException),"null")]
+        public void ConnectException()
+        {
+            Redis redis = new Redis(null, 5555);
+            redis.ConnectSocket();
+        }
+    }
+}
